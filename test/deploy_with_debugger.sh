27d2d94bc9859a25a288b8fbd91034f84b915b81
tfynes-pivotal <tfynes@Thomass-MacBook-Pro-3.local>
2019-11-21 12:10:37 -0500

    fix

--- test/deploy_with_debugger.sh
+++ test/deploy_with_debugger.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cf v3_create_app hello
-cf v3_apply_manifest -f ./hello_manifest.yml
-cf v3_push hello -p ./hello-0.0.1-SNAPSHOT.jar
+cf v3-create-app hello
+cf v3-apply-manifest -f ./hello_manifest.yml
+cf v3-push hello -p ./hello-0.0.1-SNAPSHOT.jar
 ../addAppDebuggerRoute.sh hello hellodebugger DemoSpace homelab.fynesy.com
