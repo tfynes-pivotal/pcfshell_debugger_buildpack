2702bf40d736c4abe470610a661d30adfe09a233
tfynes-pivotal <tfynes@Thomass-MacBook-Pro-3.local>
2019-11-21 12:54:39 -0500

    update

---
+++ test/deploy_with_debugger_offline.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cf v3-create-app hello
+cf v3-apply-manifest -f ./hello_manifest.yml
+cf v3-push hello -p ./hello-0.0.1-SNAPSHOT.jar
+../addAppDebuggerRoute.sh hello hellodebugger DemoSpace homelab.fynesy.com
